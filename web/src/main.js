e8a7e079e3d17c62edd36442aa7fc76775796668
Widdy Arfiansyah <widdyarfiansyah00@gmail.com>
2022-09-25 14:35:15 +0700

    chore: add apollo gql in FE

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,31 @@
-import { createApp } from "vue";
+import { createApp, h } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import authHeader from "@/service/auth-header";
+
+const httpLink = createHttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:3000/query',
+    headers: authHeader()
+})
 
 
-createApp(App).use(router).mount("#app");
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+
+const app = createApp({
+    render: () => h(App),
+})
+
+app.use(router)
+app.use(apolloProvider)
+app.mount("#app");
